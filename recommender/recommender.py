0f34696dc0157c702b33027fbb0d2fb62a7321b8
swdanielli <swli@Daniels-MacBook-Air-2.local>
2014-06-12 10:54:31 -0400

    v0.1 before user feedback

--- recommender/recommender.py
+++ recommender/recommender.py
@@ -20,7 +20,7 @@ class HelpResource(dict):
             self.update(s)
         else:
             raise TypeError("Inappropriate type "+str(type(s))+" initializing HelpResource. Should be str or dict")
-        for e,t in (('title', str), ('url', str), ('upvotes', int), ('downvotes', int), ('thumbnail', str)):
+        for e,t in (('id', int), ('title', str), ('url', str), ('upvotes', int), ('downvotes', int), ('description', str)):
             if e not in self:
                 raise TypeError("Insufficient fields initializing HelpResource. "+e+" required.")
             if not isinstance(self["e"], t):
@@ -71,29 +71,76 @@ class RecommenderXBlock(XBlock):
         data = pkg_resources.resource_string(__name__, path)
         return data.decode("utf8")
 
+    def getResourceNewId(self):
+        recoms = self.recommendations
+        if not recoms:
+            recoms = self.default_recommendations
+        resourceId = -1
+        for recom in recoms:
+            if recom['id'] > resourceId:
+                resourceId = recom['id']
+        return resourceId + 1
+
+    def getRecommendationIndex(self, recomId):
+        recoms = self.recommendations
+        if not recoms:
+            recoms = self.default_recommendations
+        for idx in range(0, len(recoms)):
+            if recoms[idx]['id'] == recomId:
+                return idx
+        return -1
+
     @XBlock.json_handler
     def handle_upvote(self, data, suffix=''):
         ''' untested '''
         resource = data['resource']
-        if resource in upvotes:
+        idx = self.getRecommendationIndex(resource)
+        if resource in self.upvotes:
+            print "Upvote failed!"
             return {"Success": False}
 
         if resource in self.downvotes:
             del self.downvotes[self.downvotes.index(resource)]
-            self.recommendations[resource]['downvotes'] -= 1
-
-        self.upvotes.append(resource)
-        self.recommendations[resource]['upvotes'] += 1
-
+            self.recommendations[idx]['downvotes'] -= 1
+        else:
+          self.upvotes.append(resource)
+          self.recommendations[idx]['upvotes'] += 1
         print "Upvote clicked!"
         return {"Success": True}
+
+    @XBlock.json_handler
+    def handle_downvote(self, data, suffix=''):
+        ''' untested '''
+        resource = data['resource']
+        idx = self.getRecommendationIndex(resource)
+        if resource in self.downvotes:
+            print "Downvote failed!"
+            return {"Success": False}
+
+        if resource in self.upvotes:
+            del self.upvotes[self.upvotes.index(resource)]
+            self.recommendations[idx]['upvotes'] -= 1
+        else:
+          self.downvotes.append(resource)
+          self.recommendations[idx]['downvotes'] += 1
+        print "Downvote clicked!"
+        return {"Success": True}
+
 
     @XBlock.json_handler
     def add_resource(self, data, suffix=''):
         ''' untested '''
         resource = data['resource']
-        
-        ## TODO: Return if already in resources
+
+        # check url for redundancy
+        recoms = self.recommendations
+        #if not recoms:
+        #    recoms = self.default_recommendations
+        for recom in recoms:
+            if recom['url'] == data['resource']['url']:
+                print "add redundant resource"
+                return {"Success": False}
+
         # Construct new resource
         valid_fields = ['url', 'title', 'description']
         new_resource = {}
@@ -101,15 +148,43 @@ class RecommenderXBlock(XBlock):
             new_resource[field] = data['resource'][field]
         new_resource['upvotes'] = 0
         new_resource['downvotes'] = 0
-        self.resources.append(new_resource)
-        return {"Success": True}
+        new_resource['id'] = self.getResourceNewId()
+        new_resource['isMisuse'] = "notMisuse"
+#        self.resources.append(new_resource)
+        self.recommendations.append(new_resource)
+        return {"Success": True, "id": new_resource['id']}
 
     @XBlock.json_handler
     def edit_resource(self, data, suffix=''):
-        pass
+        resource = data['resource']
+        idx = self.getRecommendationIndex(resource)
+        if idx not in range(0, len(self.recommendations)):
+            print "Edit failed!"
+            return {"Success": False}
+        # check url for redundancy
+        recoms = self.recommendations
+        for recom in recoms:
+            if recom['url'] == data['url']:
+                print "edit to esisting resource"
+                return {"Success": False}
+
+        for key in data:
+          if key == 'resource':
+            next
+          if data[key] == '':
+            next
+          self.recommendations[idx][key] = data[key]
+        return {"Success": True}
 
     @XBlock.json_handler
     def flag_resource(self, data, suffix=''):
+        resource = data['resource']
+        idx = self.getRecommendationIndex(resource)
+        if idx not in range(0, len(self.recommendations)):
+            print "Flag failed!"
+            return {"Success": False}
+
+        self.recommendations[idx]['isMisuse'] = data['isMisuse']
         return {"Success": True}
 
     # TO-DO: change this view to display your data your own way.
@@ -130,7 +205,8 @@ class RecommenderXBlock(XBlock):
         # TODO: Sort by votes
         # Ideally, we'd estimate score based on votes, such that items with 1 vote have a sensible ranking (rather than a perfect rating)
         # 
-        resources = [{'title' : r['title'], "votes" : r['up'] - r['down'], 'url' : r['url'], 'thumbnail' : r['thumbnail']} for r in self.recommendations]
+        resources = [{'id' : r['id'], 'title' : r['title'], "votes" : r['upvotes'] - r['downvotes'], 'url' : r['url'], 'description' : r['description'], 'isMisuse': r['isMisuse']} for r in self.recommendations]
+        resources = sorted(resources, key = lambda r: r['votes'], reverse=True)
 
         frag = Fragment(self.template_lookup.get_template("recommender.html").render(resources = resources))
         frag.add_css_url("//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css")
@@ -148,16 +224,20 @@ class RecommenderXBlock(XBlock):
         return [
             ("RecommenderXBlock",
              """<vertical_demo>
-                  <recommender> 
-                     {"title": "Covalent bonding and periodic trends", "up" : 15, "down" : 5, "url" : "https://courses.edx.org/courses/MITx/3.091X/2013_Fall/courseware/SP13_Week_4/SP13_Periodic_Trends_and_Bonding/", "thumbnail" : "http://people.csail.mit.edu/swli/edx/recommendation/img/videopage1.png"}
-                     {"title": "Polar covalent bonds and electronegativity", "up" : 10, "down" : 7, "url" : "https://courses.edx.org/courses/MITx/3.091X/2013_Fall/courseware/SP13_Week_4/SP13_Covalent_Bonding/", "thumbnail" : "http://people.csail.mit.edu/swli/edx/recommendation/img/videopage2.png"}
-                     {"title": "Longest wavelength able to to break a C-C bond ...", "up" : 10, "down" : 7, "url" : "https://answers.yahoo.com/question/index?qid=20081112142253AA1kQN1", "thumbnail" : "http://people.csail.mit.edu/swli/edx/recommendation/img/dispage1.png"}
-                     {"title": "Calculate the maximum wavelength of light for ...", "up" : 10, "down" : 7, "url" : "https://answers.yahoo.com/question/index?qid=20100110115715AA6toHw", "thumbnail" : "http://people.csail.mit.edu/swli/edx/recommendation/img/dispage2.png"}
-                     {"title": "Covalent bond - wave mechanical concep", "up" : 10, "down" : 7, "url" : "", "thumbnail" : "http://people.csail.mit.edu/swli/edx/recommendation/img/textbookpage1.png"}
-                     {"title": "Covalent bond - Energetics of covalent bond", "up" : 10, "down" : 7, "url" : "", "thumbnail" : "http://people.csail.mit.edu/swli/edx/recommendation/img/textbookpage2.png"}
+               <html_demo><img class="question" src="http://people.csail.mit.edu/swli/edx/recommendation/img/pset.png"></img></html_demo> 
+               <recommender> 
+                     {"id": 1, "title": "Covalent bonding and periodic trends", "upvotes" : 15, "downvotes" : 5, "url" : "https://courses.edx.org/courses/MITx/3.091X/2013_Fall/courseware/SP13_Week_4/SP13_Periodic_Trends_and_Bonding/", "description" : "http://people.csail.mit.edu/swli/edx/recommendation/img/videopage1.png", "isMisuse": "notMisuse"}
+                     {"id": 2, "title": "Polar covalent bonds and electronegativity", "upvotes" : 10, "downvotes" : 7, "url" : "https://courses.edx.org/courses/MITx/3.091X/2013_Fall/courseware/SP13_Week_4/SP13_Covalent_Bonding/", "description" : "http://people.csail.mit.edu/swli/edx/recommendation/img/videopage2.png", "isMisuse": "notMisuse"}
+                     {"id": 3, "title": "Longest wavelength able to to break a C-C bond ...", "upvotes" : 10, "downvotes" : 7, "url" : "https://answers.yahoo.com/question/index?qid=20081112142253AA1kQN1", "description" : "http://people.csail.mit.edu/swli/edx/recommendation/img/dispage1.png", "isMisuse": "notMisuse"}
+                     {"id": 4, "title": "Calculate the maximum wavelength of light for ...", "upvotes" : 10, "downvotes" : 7, "url" : "https://answers.yahoo.com/question/index?qid=20100110115715AA6toHw", "description" : "http://people.csail.mit.edu/swli/edx/recommendation/img/dispage2.png", "isMisuse": "notMisuse"}
                   </recommender>
                 </vertical_demo>
              """),
+#                     {"id": 5, "title": "Covalent bond - wave mechanical concep", "upvotes" : 10, "downvotes" : 7, "url" : "", "description" : "http://people.csail.mit.edu/swli/edx/recommendation/img/textbookpage1.png", "isMisuse": "notMisuse"}
+#                     {"id": 6, "title": "Covalent bond - Energetics of covalent bond", "upvotes" : 10, "downvotes" : 7, "url" : "", "description" : "http://people.csail.mit.edu/swli/edx/recommendation/img/textbookpage2.png", "isMisuse": "notMisuse"}
+#                  </recommender>
+#                </vertical_demo>
+#             """),
         ]
 
     @classmethod
--- recommender/static/js/src/recommender.js
+++ recommender/static/js/src/recommender.js
@@ -1,34 +1,251 @@
 function RecommenderXBlock(runtime, element) {
-    var handlerUrl = runtime.handlerUrl(element, 'handle_upvote');
-    
-    function voted(result) {
-        console.log("Voted");
+    var handleUpvoteUrl = runtime.handlerUrl(element, 'handle_upvote');
+    var handleDownvoteUrl = runtime.handlerUrl(element, 'handle_downvote');
+    var addResourceUrl = runtime.handlerUrl(element, 'add_resource');
+    var editResourceUrl = runtime.handlerUrl(element, 'edit_resource');
+    var flagResourceUrl = runtime.handlerUrl(element, 'flag_resource');
+
+    var baseUrl = 'http://s3-us-west-2.amazonaws.com/danielswli/'
+
+/*    POLICY_JSON = { "expiration": "2020-12-01T12:00:00.000Z",
+            "conditions": [
+            {"bucket": 'danielswli'},
+            ["starts-with", "$key", "uploads/"],
+            {"acl": "public-read"},
+            ["starts-with", "$Content-Type", ""],
+            ["content-length-range", 0, 524288000]
+            ]
+          };
+
+    var secret = 'cqAakBE0RVpl/Z5aFX8IffAhXDoIvFVSbKxvddK2';
+    var secret = this.get('AWSSecretKeyId');
+    var policyBase64 = Base64.encode(JSON.stringify(POLICY_JSON));
+    var signature = b64_hmac_sha1(secret, policyBase64);
+    b64_hmac_sha1(secret, policyBase64);*/
+
+    var policyBase64 = 'CnsiZXhwaXJhdGlvbiI6ICIyMDIwLTEyLTAxVDEyOjAwOjAwLjAwMFoiLAogICJjb25kaXRpb25zIjogWwogICAgeyJidWNrZXQiOiAiZGFuaWVsc3dsaSJ9LAogICAgWyJzdGFydHMtd2l0aCIsICIka2V5IiwgInVwbG9hZHMvIl0sCiAgICB7ImFjbCI6ICJwdWJsaWMtcmVhZCJ9LAogICAgWyJzdGFydHMtd2l0aCIsICIkQ29udGVudC1UeXBlIiwgIiJdLAogICAgWyJjb250ZW50LWxlbmd0aC1yYW5nZSIsIDAsIDUyNDI4ODAwMF0KICBdCn0=';
+    var signature = 'uRVljXwwHfM5K351eTL2MbYLwcI=';
+    $('#addResourceForm').append('<input type="hidden" name="Policy" value="' + policyBase64 + '">'
+          + '<input type="hidden" name="Signature" value="' + signature + '">'
+          + 'Previewing screenshot: <input type="file" name="file"><br>'
+          + '<input type="submit" class="submitAddResourceForm" name="submit" value="Upload File">'
+          + '<input type="button" value="Add resource" class="add_submit" disabled>');
+
+    function addResourceReset() {
+      $('.in_title').val('');
+      $('.in_url').val('')
+      $('#addResourceForm').find("input[name='file']").val('') 
+
+      var key = "uploads/" + (new Date).getTime();
+      $('#addResourceForm').find("input[name='key']").val(key);
+      $('.add_submit').attr('disabled', true);
+      $('.submitAddResourceForm').attr('disabled', false);
     }
-    
-    $('.recommender_vote_arrow_up', element).click(function(){
-        var myVote = $(this).parent().find('.recommender_vote_score').text();
-        $(this).parent().find('.recommender_vote_score').text(parseInt(myVote) + 1);
-	$.ajax({
+    addResourceReset();
+
+    $("#addResourceForm").submit( function(e) {
+      if ($('#addResourceForm').find("input[name='file']").val() == '') { return false; }
+
+      $('.add_submit').attr('disabled', false);
+      $('.submitAddResourceForm').attr('disabled', true);
+      return true;
+    });    
+
+    $('.add_submit').click(function() {
+        var data = {};
+        data['resource'] = {};
+        data['resource']['url'] = $('.in_url').val();
+        data['resource']['title'] = $('.in_title').val();
+        data['resource']['description'] = baseUrl + $(this).parent().find("input[name='key']").val();
+//        data['resource']['description'] = $(this).parent().find('.in_description').val();
+
+        $.ajax({
             type: "POST",
-            url: handlerUrl,
-            data: JSON.stringify({}),
-            success: voted
-	});
+            url: addResourceUrl,
+            data: JSON.stringify(data),
+            success: function(result) {
+              if (result['Success'] == true) {
+                $('.recommender_row').append(
+                  '<div class="recommender_resource">' +
+                  '<div class="recommender_vote_box">' +
+                  '<div class="recommender_vote_arrow_up" role="button" aria-label="upvote" tabindex="0">' +
+                  '<span class="ui-icon ui-icon-triangle-1-n"></span></div>' +
+                  '<div class="recommender_vote_score">0</div>' +
+                  '<div class="recommender_vote_arrow_down" role="button" aria-label="downvote" tabindex="0">' +
+                  '<span class="ui-icon ui-icon-triangle-1-s"></span></div>' +
+                  '</div>' + 
+                  '<div class="recommender_blurb"><div class="recommender_title">' + 
+                  data['resource']['title'] + '</div>' +
+                  '<div class="recommender_url">' + data['resource']['url'] + 
+                  '</div><div class="recommender_descriptionSlot">' + data['resource']['description'] +
+                  '</div><div class="recommender_entryId">' + result['id'] +
+                  '</div></div><div class="recommender_edit"><input type="button" value="Edit" class="editResource">' +
+                  '<br><input type="button" value="Misuse" class="flagResource notMisuse"></div></div>');
+              }
+              addResourceReset();
+              unbindEvent();
+              bindEvent();
+            }
+        });
     });
 
-    $('.recommender_blurb').click(function(){
-        var win = window.open($(this).find('.recommender_url').text(), '_blank');
-        win.focus();
-    });
+    function unbindEvent() {
+      $('.recommender_vote_arrow_up').unbind();
+      $('.recommender_vote_arrow_down').unbind();
+      $('.recommender_blurb').unbind();
+      $('.recommender_resource').unbind();
+      $('.editResource').unbind();
+      $('.flagResource').unbind();
+    }
 
-    $('.recommender_resource').hover(
-      function() {
-        $('.thumbnailImg').empty();
-        $('.thumbnailImg').append('<img src="' + $(this).find('.recommender_thumbnail').text() + '" height=100%>');
-      }, function() {
-       // $('.thumbnailImg').empty();
-      }
-    );
+    function bindEvent() {
+        $('.recommender_vote_arrow_up').click(function() {
+            var data = {};
+            data['resource'] = parseInt($(this).parent().parent().find('.recommender_entryId').text());
+//            data['resource'] = findEntry($(this).parent().parent().find('.recommender_entryId').text());
+            if (data['resource'] == -1) { return; }
+            var divArrowUp = this;
+            $.ajax({
+                type: "POST",
+                url: handleUpvoteUrl,
+                data: JSON.stringify(data),
+                success: function(result) {
+                  if (result['Success'] == true) {
+                    var scoreDiv = $(divArrowUp).parent().find('.recommender_vote_score');
+                    scoreDiv.text((parseInt(scoreDiv.text()) + 1).toString());
+                  }
+                }
+            });
+        });
+
+        $('.recommender_vote_arrow_down').click(function() {
+            var data = {};
+            data['resource'] = parseInt($(this).parent().parent().find('.recommender_entryId').text());
+//            data['resource'] = findEntry($(this).parent().parent().find('.recommender_entryId').text());
+            if (data['resource'] == -1) { return; }
+            var divArrowDown = this;
+	    $.ajax({
+                type: "POST",
+                url: handleDownvoteUrl,
+                data: JSON.stringify(data),
+                success: function(result) {
+                  if (result['Success'] == true) {
+                    var scoreDiv = $(divArrowDown).parent().find('.recommender_vote_score');
+                    scoreDiv.text((parseInt(scoreDiv.text()) - 1).toString());
+                  }
+                }
+	    });
+        });
+
+        $('.recommender_blurb').click(function(){
+          var win = window.open($(this).find('.recommender_url').text(), '_blank');
+          win.focus();
+        });
+
+        $('.recommender_resource').hover(
+          function() {
+            $('.recommender_resource').removeClass('resource_hovered');
+            $(this).addClass('resource_hovered');
+            $('.descriptionImg').empty();
+            $('.descriptionImg').append('<img src="' + $(this).find('.recommender_descriptionSlot').text() + '" height=100%>');
+          }, function() {
+          // $('.descriptionImg').empty();
+          }
+        );
+
+      $('.editResource').click(function() {
+        $('.editResource').removeClass('redTxt');
+        $(this).addClass('redTxt');
+        $('.editSourceBlock').empty();
+        var key = "uploads/" + (new Date).getTime();
+        var path = 'http://danielswli.s3.amazonaws.com/';
+        var uploadForm = '<form id="editResourceForm" action="' + path + '" method="post" enctype="multipart/form-data">'
+          + '<input type="hidden" name="key" value="' + key + '">'
+          + '<input type="hidden" name="acl" value="public-read">'
+          + '<input type="hidden" name="Content-Type" value="image/jpeg">'
+          + '<input type="hidden" name="AWSAccessKeyId" value="AKIAIRDHSV6YZJZ4RFGA">'
+          + '<input type="hidden" name="Policy" value="' + policyBase64 + '">'
+          + '<input type="hidden" name="Signature" value="' + signature + '">'
+          + 'Previewing screenshot: <input type="file" name="file"><br>'
+          + '<input type="submit" class="submitEditResourceForm" name="submit" value="Upload File">'
+          + '<input type="button" value="Edit resource" class="edit_submit" disabled></form>';
+
+        $('.editSourceBlock').append(
+          '<div class="editSourceBlockTitle">Edit selected resource</div>' +
+          'Title: ' + '<input type="text" class="edit_title"><br>' +
+          'Url: <input type="text" class="edit_url"><br>' + uploadForm);
+          //'Edited resource description: ' +
+          //'<input type="text" class="edit_description"><br>' +
+  
+        var divEdit = this;
+
+        $("#editResourceForm").submit( function(e) {
+          if ($('#editResourceForm').find("input[name='file']").val() == '') { return false; }
+
+          $('.edit_submit').attr('disabled', false);
+          $('.submitEditResourceForm').attr('disabled', 'disabled'); 
+          return true;
+        });
+
+        $('.edit_submit').click(function() {
+          var data = {};
+          data['resource'] = parseInt($(divEdit).parent().parent().find('.recommender_entryId').text());
+//          data['resource'] = findEntry($(divEdit).parent().parent().find('.recommender_entryId').text());
+          data['url'] = $('.edit_url').val();
+          data['title'] = $('.edit_title').val();
+          if (data['url'] == '' || data['title'] == '') { return; }
+
+          data['description'] = baseUrl + key;
+          $.ajax({
+              type: "POST",
+              url: editResourceUrl,
+              data: JSON.stringify(data),
+              success: function(result) {
+                if (result['Success'] == true) {
+                  $(divEdit).parent().parent().find('.recommender_title').text(data['title']);
+                  $(divEdit).parent().parent().find('.recommender_url').text(data['url']);
+                  $(divEdit).parent().parent().find('.recommender_descriptionSlot').text(data['description']);
+                  $('.editSourceBlock').empty();
+                  $('.editResource').removeClass('redTxt');
+                }
+              }
+          });
+        });
+      });
+
+      $('.flagResource').click(function() {
+        var data = {};
+        if ($(this).hasClass('notMisuse')) {
+          data['isMisuse'] = 'misuse';
+          $(this).removeClass('notMisuse').addClass('misuse');
+        }
+        else {
+          data['isMisuse'] = 'notMisuse';
+          $(this).removeClass('misuse').addClass('notMisuse');
+        }
+//        data['resource'] = findEntry($(this).parent().parent().find('.recommender_entryId').text());
+        data['resource'] = parseInt($(this).parent().parent().find('.recommender_entryId').text());
+        $.ajax({
+            type: "POST",
+            url: flagResourceUrl,
+            data: JSON.stringify(data)
+        });
+      });
+    }
+    bindEvent();
+
+    function findEntry(id) {
+      var entryId = -1;
+      $('.recommender_resource').find('.recommender_entryId').each(
+        function(idx, ele){
+          if ($(ele).text() == id) {
+            entryId = idx;
+            return;
+          }
+        }
+      );
+      return entryId;
+    }
 
 /*    $('.ui-icon-zoomin').click(function(){
         //alert($(this).parent().parent().find('.recommender_url').text());
