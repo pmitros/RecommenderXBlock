32e16a031fe7dd8df2265d728f26d90f98d8780e
Zia Fazal <zia.fazal@arbisoft.com>
2017-01-19 19:38:10 +0500

    compiling translation after installation

--- setup.py
+++ setup.py
@@ -1,7 +1,33 @@
 """Setup for recommender XBlock."""
 
 import os
+import subprocess
+from setuptools.command.install import install as _install
 from setuptools import setup
+
+
+class XBlockInstall(_install):
+    """Custom XBlock install command."""
+
+    def run(self):
+        _install.run(self)
+        self.compile_translations()
+
+    def compile_translations(self):
+        """
+        Compiles textual translations files(.po) to binary(.mo) files.
+        """
+        self.announce('Compiling translations')
+        try:
+            for dirname, _, files in os.walk(os.path.join('recommender', 'translations')):
+                for fname in files:
+                    if os.path.splitext(fname)[1] == '.po':
+                        po_path = os.path.join(dirname, fname)
+                        mo_path = os.path.splitext(po_path)[0] + '.mo'
+                        self.announce('Compiling translation at %s' % po_path)
+                        subprocess.check_call(['msgfmt', po_path, '-o', mo_path], cwd=self.install_lib)
+        except Exception as ex:
+            self.announce('Translations compilation failed: %s', ex.message)
 
 
 def package_data(pkg, root_list):
@@ -28,4 +54,7 @@ setup(
         ]
     },
     package_data=package_data("recommender", ["static", "translations"]),
+    cmdclass={
+        'install': XBlockInstall,
+    },
 )
