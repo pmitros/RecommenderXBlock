afd00cd387de685ba2fa601fd4c71a5f85dddd68
Feanil Patel <feanil@edx.org>
2019-09-23 15:10:30 -0400

    Update version.

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ def package_data(pkg, root_list):
 
 setup(
     name='recommender-xblock',
-    version='1.4.3',
+    version='1.4.4',
     description='recommender XBlock',   # TODO: write a better description.
     long_description=README,
     author='edX',
